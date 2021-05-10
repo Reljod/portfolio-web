581dd17ea3ea3889886bbbfd7088b3d14a38646c
reljod <oretareljod@gmail.com>
2021-05-10 11:27:32 +0800

    enh: added introduction component and landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container mx-auto bg-gray-800">
+    <Introduction />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Introduction from "@/components/Introduction.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Introduction,
   },
 };
 </script>
